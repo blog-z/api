b6ac145b9fb36cfbb0fbb71682e254c90e636f9c
zhuqingchun <729512117@qq.com>
2019-09-29 22:27:58 +0800

    bug

--- src/main/java/com/dubbo/commons/Const.java
+++ src/main/java/com/dubbo/commons/Const.java
@@ -5,5 +5,6 @@ public class Const {
     public interface Role{
         String ROLE_CONSUMER="1";    //普通用户
         String ROLE_ADMIN="0";   //管理员
+        String COMMENT_ID="commentId";
     }
 }
