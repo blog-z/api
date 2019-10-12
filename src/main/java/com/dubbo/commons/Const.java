a5c8cb7b6b854f022a467578b9124afd9479fae6
zhuqingchun <729512117@qq.com>
2019-10-12 22:53:22 +0800

    maven编译8

--- src/main/java/com/dubbo/commons/Const.java
+++ src/main/java/com/dubbo/commons/Const.java
@@ -7,4 +7,11 @@ public class Const {
         String ROLE_ADMIN="0";   //管理员
         String COMMENT_ID="commentId";
     }
+
+    public interface RedisKey{
+        //防止存入实体信息是不同类型实体的key值发生冲突,所以加上前缀
+        String BeforeUserKeyId="BeforeUserKeyId";
+        String BeforeArticleKeyId="BeforeArticleKeyId";
+        String BeforeCommentKeyId="BeforeCommentKeyId";
+    }
 }
